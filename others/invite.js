ccca330c25217ba7d231900749dc1ecc559f73cf
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-10-23 15:22:15 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -13,9 +13,9 @@ module.exports = {
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<:emoji_81:881156629742096394>");
+    message.react("<:emoji_55:900461622185828492>");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#FF55FF").setTitle("").setDescription("<a:zeko:837426841408110653> **[Click Here](https://discord.com/api/oauth2/authorize?client_id=881148979373555763&permissions=261724045121&scope=bot) to invite the bot.**")
+    message.reply(new MessageEmbed().setColor("#FF55FF").setTitle("").setDescription("<:emoji_55:900461622185828492> **[Click Here](https://discord.com/api/oauth2/authorize?client_id=881148979373555763&permissions=261724045121&scope=bot) to invite the bot.**")
     .setFooter(""));
   }
 }
